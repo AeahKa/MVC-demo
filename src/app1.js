c147961c344d4c715590868e1f19fe27e83c6d82
AeahKa <aeah@qq.com>
2022-11-15 19:27:23 +0800

    init

---
+++ src/app1.js
@@ -0,0 +1,62 @@
+import $ from 'jquery'
+import './app1.css'
+import Model from './base/Model.js'
+import View from './base/View.js'
+import EventBus from './base/EventBus'
+
+// 数据相关都放到 m
+const m = new Model({
+	data: {
+		n: parseFloat(localStorage.getItem('n')),
+	},
+	update: function (data) {
+		Object.assign(m.data, data)
+		m.trigger('m:update')
+		localStorage.setItem('n', m.data.n)
+	},
+})
+
+const init = (el) => {
+	new View({
+		el: el,
+		data: m.data,
+		html: `
+			<div>
+				<div class="output">
+					<span id="number">{{n}}</span>
+				</div>
+				<div class="action">
+					<button id="plus1">+1</button>
+					<button id="minus1">-1</button>
+					<button id="mul2">*2</button>
+					<button id="divide2">÷2</button>
+				</div>
+			</div>
+		`,
+		render(data) {
+			const n = data.n
+			if (this.el.children.length !== 0) this.el.empty()
+			$(this.html.replace('{{n}}', n)).appendTo(this.el)
+		},
+		events: {
+			'click #plus1': 'plus',
+			'click #minus1': 'minus',
+			'click #mul2': 'mul',
+			'click #divide2': 'divide',
+		},
+		plus() {
+			m.update({ n: m.data.n + 1 })
+		},
+		minus() {
+			m.update({ n: m.data.n - 1 })
+		},
+		mul() {
+			m.update({ n: m.data.n * 2 })
+		},
+		divide() {
+			m.update({ n: m.data.n / 2 })
+		},
+	})
+}
+
+export default init
---
+++ src/app2.js
@@ -0,0 +1,56 @@
+import './app2.css'
+import $ from 'jquery'
+import Model from './base/Model.js'
+import View from './base/View.js'
+
+const localKey = 'app2.index'
+const m = new Model({
+	data: {
+		index: parseInt(localStorage.getItem(localKey)) || 0,
+	},
+	update: function (data) {
+		Object.assign(m.data, data)
+		eventBus.trigger('m:updated')
+		localStorage.setItem(localKey, m.data.index)
+	},
+})
+
+const init = (el) => {
+	new View({
+		el: el,
+		data: m.data,
+		eventBus: eventBus,
+		html: (index) => {
+			return `
+			<div>
+				<ol class="tab-bar">
+					<li class="${
+						index === 0 ? 'selected' : ''
+					}" data-index="0"><span>1111</span></li>
+					<li class="${
+						index === 1 ? 'selected' : ''
+					}" data-index="1"><span>2222</span></li>
+				</ol>
+				<ol class="tab-content">
+					<li class="${index === 0 ? 'active' : ''}">内容1</li>
+					<li class="${index === 1 ? 'active' : ''}">内容2</li>
+				</ol>
+			</div>
+		`
+		},
+		render(data) {
+			const index = data.index
+			if (this.el.children.length !== 0) this.el.empty()
+			$(this.html(index)).appendTo(this.el)
+		},
+		events: {
+			'click .tab-bar li': 'x',
+		},
+		x(e) {
+			const index = parseInt(e.currentTarget.dataset.index)
+			m.update({ index: index })
+		},
+	})
+}
+
+export default init
